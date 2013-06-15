6b8b5cfd005690a4855c3ada6d48ec508650c701
Alex Klinkhamer <grencez@codelove.org>
2013-06-15 16:24:04 -0400

    Replace BCasc() with LegitCk() macro
    1. Lots of lines are changed due to re-indenting.
       This would be unavoidable even if I hadn't switched
       from 4 to 2 spaces per indent level.
      c. chatty.c
      c. waitdo.c

--- chatty.c
+++ chatty.c
@@ -25,171 +25,161 @@
 
 int main (int argc, char** argv)
 {
-    int argi =
-        (init_sysCx (&argc, &argv),
-         1);
-    bool good = true;
-    int istat = 0;
-    const char* host = "127.0.0.1";
-    const char* service = "1337";
+  int argi =
+    (init_sysCx (&argc, &argv),
+     1);
+  bool good = true;
+  bool bstat = true;
+  int istat = 0;
+  const char* host = "127.0.0.1";
+  const char* service = "1337";
 
-    struct addrinfo crit;
-    struct addrinfo* list = 0;
-    struct addrinfo* addr = 0;
-    DecloStack( struct aiocb, aio );
+  struct addrinfo crit;
+  struct addrinfo* list = 0;
+  struct addrinfo* addr = 0;
+  DecloStack( struct aiocb, aio );
 
-    memset (&crit, 0, sizeof (crit));
-    memset (aio, 0, sizeof (*aio));
+  memset (&crit, 0, sizeof (crit));
+  memset (aio, 0, sizeof (*aio));
 
-        /* crit.ai_family = AF_INET6; */
-    crit.ai_family   = AF_INET;
-        /* crit.ai_flags    = AI_V4MAPPED | AI_ADDRCONFIG; */
-    crit.ai_socktype = SOCK_STREAM;
-    crit.ai_protocol = IPPROTO_TCP;
-        /* crit.ai_family   = AF_UNSPEC; */
-        /* crit.ai_flags    = AI_PASSIVE; */
-        /* crit.ai_socktype = SOCK_DGRAM; */
-        /* crit.ai_protocol = IPPROTO_UDP; */
+  /* crit.ai_family = AF_INET6; */
+  crit.ai_family   = AF_INET;
+  /* crit.ai_flags    = AI_V4MAPPED | AI_ADDRCONFIG; */
+  crit.ai_socktype = SOCK_STREAM;
+  crit.ai_protocol = IPPROTO_TCP;
+  /* crit.ai_family   = AF_UNSPEC; */
+  /* crit.ai_flags    = AI_PASSIVE; */
+  /* crit.ai_socktype = SOCK_DGRAM; */
+  /* crit.ai_protocol = IPPROTO_UDP; */
 
 
-    if (argi < argc && !eql_cstr (argv[argi], "-connect"))
-        failout_sysCx ("I take no arguments from humans.");
+  if (argi < argc && !eql_cstr (argv[argi], "-connect"))
+    failout_sysCx ("I take no arguments from humans.");
 
-    BInit();
-
-    istat = getaddrinfo (host, service, &crit, &list);
-    BCasc( istat == 0, good, "getaddrinfo()" );
-
+  istat = getaddrinfo (host, service, &crit, &list);
+  if (LegitCk( istat == 0, good, "getaddrinfo()" ))
+  {
     addr = list;
     if (addr->ai_next)
     {
-        DBog0( "Host could bind to multiple!" );
+      DBog0( "Host could bind to multiple!" );
     }
-
     if (argi < argc && eql_cstr (argv[argi], "-connect"))
     {
-        int sock = -1;
-        DecloStack1( OFileB, of, dflt_OFileB () );
+      int sock = -1;
+      DecloStack1( OFileB, of, dflt_OFileB () );
 
-        BInit();
+      oput_cstr_OFileB (of, "hi");
 
-        oput_cstr_OFileB (of, "hi");
+      /* Wait for parent proc to be ready.*/
+      xget_XFileB (stdin_XFileB ());
+      close_XFileB (stdin_XFileB ());
 
-        /* Wait for parent proc to be ready.*/
-        xget_XFileB (stdin_XFileB ());
-        close_XFileB (stdin_XFileB ());
-
-        sock = socket (addr->ai_family,
-                       addr->ai_socktype,
-                       addr->ai_protocol);
-        BCasc( sock >= 0, good, "socket()" );
-
+      sock = socket (addr->ai_family,
+                     addr->ai_socktype,
+                     addr->ai_protocol);
+      if (LegitCk( sock >= 0, good, "socket()" ))
         istat = connect (sock, addr->ai_addr, addr->ai_addrlen);
-        BCasc( istat >= 0, good, "connect()" );
-
+      if (LegitCk( istat >= 0, good, "connect()" ))
+      {
         aio->aio_fildes = sock;
         aio->aio_buf = of->buf.s;
         aio->aio_nbytes = of->off;
         istat = aio_write (aio);
-        BCasc( istat == 0, good, "aio_write()" );
-
+      }
+      if (LegitCk( istat == 0, good, "aio_write()" ))
+      {
         do {
-            const struct aiocb* tmp = aio;
-            istat = aio_suspend (&tmp, 1, 0);
+          const struct aiocb* tmp = aio;
+          istat = aio_suspend (&tmp, 1, 0);
         } while (istat != 0 && errno == EINTR);
+      }
+      if (LegitCk( istat == 0, good, "aio_suspend()" ))
+      {}
+      if (LegitCk( aio_return (aio) == (ssize_t) aio->aio_nbytes, good, "aio_return()" ))
+      {}
 
-        BCasc( istat == 0, good, "aio_suspend()" );
-        BCasc( aio_return (aio) == (ssize_t) aio->aio_nbytes, good, "aio_return()" );
-
-        BLose();
-
-        if (sock >= 0)  close (sock);
-        lose_OFileB (of);
+      if (sock >= 0)  close (sock);
+      lose_OFileB (of);
     }
     else
     {
-        DecloStack1( OSPc, ospc, dflt_OSPc () );
-        int sock = -1;
-        int sock1 = -1;
-        struct sockaddr_storage client_addr;
-        socklen_t client_addr_nbytes = sizeof (client_addr);
-        DeclTable( byte, msg );
+      DecloStack1( OSPc, ospc, dflt_OSPc () );
+      int sock = -1;
+      int sock1 = -1;
+      struct sockaddr_storage client_addr;
+      socklen_t client_addr_nbytes = sizeof (client_addr);
+      DeclTable( byte, msg );
 
-        BInit();
+      ospc->cmd = cons1_AlphaTab (exename_of_sysCx ());
+      PushTable( ospc->args, cons1_AlphaTab ("-connect") );
 
-        ospc->cmd = cons1_AlphaTab (exename_of_sysCx ());
-        PushTable( ospc->args, cons1_AlphaTab ("-connect") );
-
-        stdxpipe_OSPc (ospc);
-        good = spawn_OSPc (ospc);
-        BCasc( istat == 0, good, "spawn()" );
-
+      stdxpipe_OSPc (ospc);
+      bstat = spawn_OSPc (ospc);
+      if (LegitCk( bstat, good, "spawn()" ))
+      {
         EnsizeTable( msg, 1024 );
 
         sock = socket (addr->ai_family,
                        addr->ai_socktype,
                        addr->ai_protocol);
-        BCasc( sock >= 0, good, "socket()" );
-
+      }
+      if (LegitCk( sock >= 0, good, "socket()" ))
         istat = bind (sock, addr->ai_addr, addr->ai_addrlen);
-        BCasc( istat == 0, good, "bind()" );
 
+      if (LegitCk( istat == 0, good, "bind()" ))
         istat = listen (sock, SOMAXCONN);
-        BCasc( istat == 0, good, "listen()" );
 
+      if (LegitCk( istat == 0, good, "listen()" ))
+      {
         /* Tell spawned process that we are listening.*/
         close_OFileB (ospc->of);
 
         sock1 = accept (sock,
                         (struct sockaddr*) &client_addr,
                         &client_addr_nbytes);
-
-        BCasc( sock1 >= 0, good, "accept()" );
-
+      }
+      if (LegitCk( sock1 >= 0, good, "accept()" ))
+      {
         aio->aio_fildes = sock1;
         aio->aio_buf = msg.s;
         aio->aio_nbytes = msg.sz;
 
-        aio_read (aio);
-
-        BCasc( sock1 >= 0, good, "aio_read()" );
+        istat = aio_read (aio);
+      }
+      if (LegitCk( istat == 0, good, "aio_read()" ))
+      {
         do {
-            const struct aiocb* tmp = aio;
-            istat = aio_suspend (&tmp, 1, 0);
+          const struct aiocb* tmp = aio;
+          istat = aio_suspend (&tmp, 1, 0);
         } while (istat < 0 && errno == EINTR);
+      }
+      if (LegitCk( istat == 0, good, "aio_suspend()" ))
+      {
+        ssize_t nbytes = aio_return (aio);
 
-        BCasc( istat == 0, good, "aio_suspend()" );
-
+        if (LegitCk( nbytes > 0, good, "aio_return()" ))
         {
-            ssize_t nbytes = aio_return (aio);
-            BInit();
-            BCasc( nbytes > 0, good, "aio_return()" );
-            msg.sz = nbytes;
-            BLose();
+          msg.sz = nbytes;
+          fputs ("got:", stdout);
+          fwrite (msg.s, sizeof(char), msg.sz, stdout);
+          fputc ('\n', stdout);
         }
-        BCasc( 1, good, 0 );
+      }
 
-        fputs ("got:", stdout);
-        fwrite (msg.s, sizeof(char), msg.sz, stdout);
-        fputc ('\n', stdout);
+      LoseTable( msg );
+      lose_OSPc (ospc);
 
-        BLose();
-
-        LoseTable( msg );
-        lose_OSPc (ospc);
-
-        if (sock1 >= 0)  close (sock1);
-        if (sock >= 0)  close (sock);
+      if (sock1 >= 0)  close (sock1);
+      if (sock >= 0)  close (sock);
     }
-    BCasc( 1, good, 0 );
+  }
 
-    BLose();
+  if (list)
+    freeaddrinfo (list);
 
-    if (list)
-        freeaddrinfo (list);
-
-    memset (aio, 0, sizeof (*aio));
-    lose_sysCx ();
-    return 0;
+  memset (aio, 0, sizeof (*aio));
+  lose_sysCx ();
+  return 0;
 }
 
--- waitdo.c
+++ waitdo.c
@@ -8,47 +8,44 @@
 
 int main (int argc, char** argv)
 {
-    int argi =
-        (init_sysCx (&argc, &argv),
-         1);
-    const char* ExeName = argv[0];
-    FILE* in = stdin;
-    FILE* ErrOut = stderr;
-    bool good = true;
+  int argi =
+    (init_sysCx (&argc, &argv),
+     1);
+  const char* ExeName = argv[0];
+  FILE* in = stdin;
+  FILE* ErrOut = stderr;
+  bool good = true;
 
-    BInit();
-
-    BCasc( argi < argc, good, 0 );
-
+  if (LegitCk( argi < argc, good, 0 ))
+  {
     if (0 == strcmp (argv[argi], "-x"))
     {
-        ++ argi;
-        if (argi < argc)
-            in = fopen (argv[argi], "rb");
+      ++ argi;
+      if (argi < argc)
+        in = fopen (argv[argi], "rb");
 
-        ++ argi;
+      ++ argi;
     }
-
-    BCasc( in, good, "File open." );
-
-    BCasc( argi < argc, good, 0 );
-
+  }
+  if (LegitCk( in, good, "File open." ))
+  {}
+  if (LegitCk( argi < argc, good, 0 ))
+  {
     if (0 == strcmp (argv[argi], "--"))  ++ argi;
-
-    BCasc( argi < argc, good, "Need a command!" );
-
+  }
+  if (LegitCk( argi < argc, good, "Need a command!" ))
+  {
     while (! feof (in) && ! ferror (in))  fgetc (in);
     fclose (in);
 
     execvp_sysCx (&argv[argi]);
 
     fprintf (ErrOut, "%s - Failed to execute:%s\n", ExeName, argv[2]);
+  }
 
-    BLose();
+  fprintf (ErrOut, "Usage: %s [-x IN] [--] COMMAND [ARG...]\n", ExeName);
 
-    fprintf (ErrOut, "Usage: %s [-x IN] [--] COMMAND [ARG...]\n", ExeName);
-
-    lose_sysCx ();
-    return 1;
+  lose_sysCx ();
+  return 1;
 }
 
