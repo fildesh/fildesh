2402f56d1e30412f701d678d5b99caa7d2d35196
Alex Klinkhamer <grencez@gmail.com>
2021-06-13 04:57:30 -0700

    Add `shout` tool that prints to a file
    No test with it yet.
    https://github.com/grencez/lace/issues/27

---
+++ tool/shout.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/** Write to a file (1st argument) with content (2nd+ arguments).
+ *
+ * The content is separated by spaces and ends with a newline.
+ * Keep small; built often.
+ **/
+int main(int argc, char** argv) {
+  FILE* f = stdout;
+  int argi;
+  if (argc < 2 || !argv[1]) {
+    return 64;  /* EX_USAGE: Command line usage error.*/
+  }
+  if (argv[1][0] != '-' || argv[1][1] != '\0') {
+    f = fopen(argv[1], "wb");
+    if (!f)  return 73;  /* EX_CANTCREAT: Cannot open output file.*/
+  }
+
+  for (argi = 2; argi < argc && argv[argi]; ++argi) {
+    if (argi > 2 && 0 > fputc(' ', f)) {
+      return 74;  /* EX_IOERR: A write failed.*/
+    }
+    if (0 > fputs(argv[argi], f))  return 74;
+  }
+  if (0 > fputc('\n', f))  return 74;
+  if (0 != fclose(f))  return 74;
+  return 0;  /* Success!*/
+}
