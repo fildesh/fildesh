861f4d697e72be20046b6563eddf830a06b26ff3
grencez <git@grencez.dev>
2022-11-26 01:52:13 -0800

    Plug leak in //test/compat:tmpdir_test

--- test/compat/tmpdir_test.c
+++ test/compat/tmpdir_test.c
@@ -1,6 +1,7 @@
 
 #include "fildesh_compat_file.h"
 #include <assert.h>
+#include <stdlib.h>
 
 int main() {
   char* d = fildesh_compat_file_mktmpdir("fildeshtmpdirtest");
@@ -8,5 +9,6 @@ int main() {
   assert(d);
   istat = fildesh_compat_file_rmdir(d);
   assert(istat == 0 && "tmpdir removal");
+  free(d);
   return 0;
 }
